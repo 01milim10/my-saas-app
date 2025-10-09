4c1de15ddf8c8287a17926e318fa651fdfddf288
Milan Budhathoki <mbudhathoki1996@gmail.com>
2025-10-09 18:41:07 -0500

    Implement User Profile

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://132e877846a2dbf3e7612d46c37567fd@o4510128320479232.ingest.us.sentry.io/4510161471930368",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
